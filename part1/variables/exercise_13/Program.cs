c39ba672ddf2161f9dbf9c547373e6ea8489ab9f
JT Sousa <joao.sousa@centria.fi>
2020-01-23 19:34:27 +0200

    exercise 13 done

--- part1/variables/exercise_13/Program.cs
+++ part1/variables/exercise_13/Program.cs
@@ -7,6 +7,10 @@ namespace exercise_13
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number!");
+      string number = Console.ReadLine();
+      double duplo = Convert.ToDouble(number);
+      Console.WriteLine("You gave " + duplo);
 
     }
   }
