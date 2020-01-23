83074417435b991060bd710141ce4142159a26d9
JT Sousa <joao.sousa@centria.fi>
2020-01-23 20:07:24 +0200

    exercise 15 done

--- part1/variables/exercise_15/Program.cs
+++ part1/variables/exercise_15/Program.cs
@@ -8,6 +8,21 @@ namespace exercise_15
     {
 
       // Write your code here:
+      Console.WriteLine("Give a string:");
+      string userString = Console.ReadLine();
+      Console.WriteLine("Give an integer:");
+      string userInteger = Console.ReadLine();
+      int realInteger = Convert.ToInt32(userInteger);
+      Console.WriteLine("Give a double:");
+      string userDouble = Console.ReadLine();
+      double realDouble = Convert.ToDouble(userDouble);
+      Console.WriteLine("Give a boolean:");
+      string userBoolean = Console.ReadLine();
+      bool realBool = System.Convert.ToBoolean(userBoolean);
+      Console.WriteLine("Your string: " + userString);
+      Console.WriteLine("Your integer: " + realInteger);
+      Console.WriteLine("Your double: " + realDouble);
+      Console.WriteLine("Your boolean: " + realBool);
 
     }
   }
