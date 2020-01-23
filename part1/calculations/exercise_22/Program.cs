3357c7bf7caad03a8d11573797de28e3a7b74755
JT Sousa <joao.sousa@centria.fi>
2020-01-23 22:17:29 +0200

    exercise 22 done

--- part1/calculations/exercise_22/Program.cs
+++ part1/calculations/exercise_22/Program.cs
@@ -8,6 +8,17 @@ namespace exercise_22
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string firstNumber = Console.ReadLine();
+      int firstInt = Convert.ToInt32(firstNumber);
+      Console.WriteLine("Give the second number!");
+      string secondNumber = Console.ReadLine();
+      int secondInt = Convert.ToInt32(secondNumber);
+      Console.WriteLine("Give the third number!");
+      string thirdNumber = Console.ReadLine();
+      int thirdInt = Convert.ToInt32(thirdNumber);
+      double average = (firstInt + secondInt + thirdInt) / 3.0;
+      Console.WriteLine("The average is " + average);
 
     }
   }
