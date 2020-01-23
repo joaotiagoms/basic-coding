9ff2be8d9cc22ca4a91c249fca63042537b4249e
JT Sousa <joao.sousa@centria.fi>
2020-01-23 22:33:45 +0200

    exercise 23 done

--- part1/calculations/exercise_23/Program.cs
+++ part1/calculations/exercise_23/Program.cs
@@ -8,6 +8,18 @@ namespace exercise_23
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string firstNumber = Console.ReadLine();
+      int firstInt = Convert.ToInt32(firstNumber);
+      Console.WriteLine("Give the second number!");
+      string secondNumber = Console.ReadLine();
+      int secondInt = Convert.ToInt32(secondNumber);
+      Console.WriteLine(firstInt + " + " + secondInt + " = " + (firstInt+secondInt));
+      Console.WriteLine(firstInt + " - " + secondInt + " = " + (firstInt-secondInt));
+      Console.WriteLine(firstInt + " * " + secondInt + " = " + (firstInt*secondInt));
+      double firstDoub = Convert.ToDouble(firstNumber);
+      double secondDoub = Convert.ToDouble(secondNumber);
+      Console.WriteLine(firstDoub + " / " + secondDoub + " = " + (firstDoub/secondDoub));
 
     }
   }
