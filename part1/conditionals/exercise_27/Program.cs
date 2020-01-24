700851e3c661241ea3d399a9165abe693837ee1e
JT Sousa <joao.sousa@centria.fi>
2020-01-24 10:46:07 +0200

    exercise 27 done

--- part1/conditionals/exercise_27/Program.cs
+++ part1/conditionals/exercise_27/Program.cs
@@ -7,6 +7,17 @@ namespace exercise_27
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number:");
+      string number = Console.ReadLine();
+      int numberInt = Convert.ToInt32(number);
+      if (numberInt <= 0)
+      {
+        Console.WriteLine("It is not positive");
+      }
+      else
+      {
+        Console.WriteLine("It is positive");
+      }
 
     }
   }
