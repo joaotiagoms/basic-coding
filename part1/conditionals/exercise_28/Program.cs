82423d2dcc70f44ad1b584fb80f334ea3d8d8415
JT Sousa <joao.sousa@centria.fi>
2020-01-24 10:53:47 +0200

    exercise 28 done

--- part1/conditionals/exercise_28/Program.cs
+++ part1/conditionals/exercise_28/Program.cs
@@ -7,6 +7,18 @@ namespace exercise_28
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("How old are you?");
+      string age = Console.ReadLine();
+      int ageInt = Convert.ToInt32(age);
+      if (ageInt >= 18)
+      {
+         Console.WriteLine("You're an adult!");
+      }
+      else
+      {
+          Console.WriteLine("You're under age!");
+      }
+
 
     }
   }
