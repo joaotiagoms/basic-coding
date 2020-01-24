bf3d0d64a19b78d433add45198bebb129b8172da
JT Sousa <joao.sousa@centria.fi>
2020-01-24 11:38:24 +0200

    exercise 31 done

--- part1/conditionals/exercise_31/Program.cs
+++ part1/conditionals/exercise_31/Program.cs
@@ -7,6 +7,17 @@ namespace exercise_31
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number:");
+      string number = Console.ReadLine();
+      int numberInt = Convert.ToInt32(number);
+      if (numberInt % 2 == 0)
+      {
+          Console.WriteLine("It is even.");
+      }
+      else
+      {
+          Console.WriteLine("It is odd.");
+      }
 
     }
   }
