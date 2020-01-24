060fc135849484afd4fb8f224a9dd69fce20693f
JT Sousa <joao.sousa@centria.fi>
2020-01-24 10:16:18 +0200

    exercise 25 done

--- part1/conditionals/exercise_25/Program.cs
+++ part1/conditionals/exercise_25/Program.cs
@@ -7,6 +7,13 @@ namespace exercise_25
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number:");
+      string number = Console.ReadLine();
+      int numberInt = Convert.ToInt32(number);
+      if (numberInt == 1984)
+      {
+        Console.WriteLine("Orwell");
+      }
 
     }
   }
