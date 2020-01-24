095c2b2f300175d43146752d7d1138095d915b7b
JT Sousa <joao.sousa@centria.fi>
2020-01-24 13:27:47 +0200

    exercise 34 done

--- part1/repetition/exercise_34/Program.cs
+++ part1/repetition/exercise_34/Program.cs
@@ -7,6 +7,12 @@ namespace exercise_34
     public static void Main(string[] args)
     {
       // Write your code here:
+      string answer = ("start");
+      while (answer != ("no"))
+      {
+        Console.WriteLine("Do you want to continue?");
+        answer = Console.ReadLine();
+      }
 
     }
   }
