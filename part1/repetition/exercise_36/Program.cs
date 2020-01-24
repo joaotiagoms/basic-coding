fe60b54511064212b225e1bba504318be27cb010
JT Sousa <joao.sousa@centria.fi>
2020-01-24 14:04:55 +0200

    exercise 36 done

--- part1/repetition/exercise_36/Program.cs
+++ part1/repetition/exercise_36/Program.cs
@@ -7,6 +7,23 @@ namespace exercise_36
     public static void Main(string[] args)
     {
       // Write your code here:
+      int numba = 1;
+      while (numba !=0)
+      {
+        Console.WriteLine("Give a number:");
+        numba = Convert.ToInt32(Console.ReadLine());
+        if (numba < 0)
+        {
+          Console.WriteLine("That is negative");
+        }
+        else if (numba > 0)
+        {
+            Console.WriteLine(numba*numba);
+        }
+
+
+
+      }
 
     }
   }
