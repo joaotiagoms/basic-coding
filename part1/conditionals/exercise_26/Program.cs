37685f5121af2d3f6733fb482d75cd16104609e8
JT Sousa <joao.sousa@centria.fi>
2020-01-24 10:20:55 +0200

    exercise 26 done

--- part1/conditionals/exercise_26/Program.cs
+++ part1/conditionals/exercise_26/Program.cs
@@ -7,6 +7,13 @@ namespace exercise_26
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give your age:");
+      string age = Console.ReadLine();
+      int ageInt = Convert.ToInt32(age);
+      if (ageInt < 1900)
+      {
+        Console.WriteLine("You're old");
+      }
 
     }
   }
