8af5832e12617118484bb44e4395fe9a8c43a71d
JT Sousa <joao.sousa@centria.fi>
2020-01-24 13:54:18 +0200

    exercise 35 done

--- part1/repetition/exercise_35/Program.cs
+++ part1/repetition/exercise_35/Program.cs
@@ -7,6 +7,12 @@ namespace exercise_35
     public static void Main(string[] args)
     {
       // Write your code here:
+      int fortyTwo = 0;
+      while (fortyTwo != 42)
+      {
+        Console.WriteLine("Give a number:");
+        fortyTwo = Convert.ToInt32(Console.ReadLine());
+      }
 
     }
   }
