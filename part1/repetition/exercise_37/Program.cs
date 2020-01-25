12b58d09d67338b110b7f5a6dcd98fcabfdd6f92
JT Sousa <joao.sousa@centria.fi>
2020-01-25 11:57:40 +0200

    exercise 37 done

--- part1/repetition/exercise_37/Program.cs
+++ part1/repetition/exercise_37/Program.cs
@@ -8,6 +8,14 @@ namespace exercise_37
     {
       int sum = 0;
       // Write your code here:
+      int answer = 1;
+      while (answer != 0)
+      {
+        Console.WriteLine("Give a number:");
+        answer = Convert.ToInt32(Console.ReadLine());
+        sum = sum + 1;
+      }
+      Console.WriteLine("Total amount of numbers: " + (sum - 1));
 
     }
   }
