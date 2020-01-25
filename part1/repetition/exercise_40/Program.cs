f05c475167644647008c199d351138d1a62e96ce
JT Sousa <joao.sousa@centria.fi>
2020-01-25 13:09:03 +0200

    exercise 40 done

--- part1/repetition/exercise_40/Program.cs
+++ part1/repetition/exercise_40/Program.cs
@@ -8,6 +8,21 @@ namespace exercise_40
     {
 
       // Write your code here:
+      int sum = 0;
+      int answer = 0;
+      int userAnswer = 1;
+      while (userAnswer != 0)
+      {
+        Console.WriteLine("Give a number:");
+        userAnswer = Convert.ToInt32(Console.ReadLine());
+        if (userAnswer != 0)
+        {
+          answer = answer + userAnswer;
+          sum = sum + 1;
+        }
+      }
+      Console.WriteLine("Total sum of numbers: " + answer);
+      Console.WriteLine("Total amount of numbers: " + sum);
     }
   }
 }
