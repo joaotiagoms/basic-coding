ad1fe5596e57285b5b2b194e595517ce627d501e
JT Sousa <joao.sousa@centria.fi>
2020-01-28 20:55:41 +0200

    exercise 45 done

--- part2/moreLoops/exercise_45/Program.cs
+++ part2/moreLoops/exercise_45/Program.cs
@@ -8,7 +8,17 @@ namespace exercise_45
     {
 
       // Write your code here:
-
+      int output1 = 0;
+      int input1 = Convert.ToInt32(Console.ReadLine());
+      while (true)
+      {
+        if (output1 > input1)
+        {
+        break;
+        }
+        Console.WriteLine(output1);
+        output1 = output1 + 1;
+      }
     }
   }
 }
