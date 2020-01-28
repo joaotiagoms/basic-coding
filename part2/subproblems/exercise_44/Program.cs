57e30619a026e3bcff5e612936c27d9968f54aff
JT Sousa <joao.sousa@centria.fi>
2020-01-28 18:42:10 +0200

    exercise 44 done

--- part2/subproblems/exercise_44/Program.cs
+++ part2/subproblems/exercise_44/Program.cs
@@ -8,6 +8,18 @@ namespace exercise_44
     {
 
       // Write your code here:
+      int input1 = Convert.ToInt32(Console.ReadLine());
+      int input2 = Convert.ToInt32(Console.ReadLine());
+      if (input1 > input2)
+      {
+        Console.WriteLine(input1 + " is greater than " + input2 + ".");
+      }
+      else if (input2 > input1)
+      {
+        Console.WriteLine(input1 + " is less than " + input2 + ".");
+      }
+      else
+        Console.WriteLine(input1 + " is equal to " + input2 + ".");
 
     }
   }
