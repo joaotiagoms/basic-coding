bcd00e7df859a6e4df863edfb5fb3cdfa969e710
JT Sousa <joao.sousa@centria.fi>
2020-02-01 12:42:55 +0200

    exercise 50 done

--- part2/methods/exercise_50/Program.cs
+++ part2/methods/exercise_50/Program.cs
@@ -7,12 +7,24 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
+      Console.WriteLine("How many times?");
+      int numTimes = Convert.ToInt32(Console.ReadLine());
+      int i = 0;
+      while (i < numTimes)
+      {
+      PrintPhrase();  
+      i++;
+      }
+      
  
     }
 
     // Write your method here:
+    
     public static void PrintPhrase() {
 
+      Console.WriteLine("In a hole in the ground there lived a method");
+    }
     }
   }
-}
+
