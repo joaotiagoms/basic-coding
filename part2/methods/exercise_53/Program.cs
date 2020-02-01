e3fc39291180922627bfb0c8d4d61f3f557b2196
JT Sousa <joao.sousa@centria.fi>
2020-02-01 21:33:49 +0200

    exercise 53 done

--- part2/methods/exercise_53/Program.cs
+++ part2/methods/exercise_53/Program.cs
@@ -7,11 +7,15 @@ namespace exercise_53
     public static void Main(String[] args)
     {
       // Call your method here:
-
-
+      Division(7,3);
     }
 
     // Write your method here:
+    public static void Division(int numerator, int denominator)
+    {
+      Console.WriteLine((Convert.ToDouble(numerator))/(Convert.ToDouble(denominator)));
+    }
+    
 
   }
 }
