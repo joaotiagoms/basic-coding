9af0298e060b1f1a3120acf9994bf69fb8669502
JT Sousa <joao.sousa@centria.fi>
2020-02-01 11:25:57 +0200

    exercise 49 done

--- part2/methods/exercise_49/Program.cs
+++ part2/methods/exercise_49/Program.cs
@@ -6,13 +6,15 @@ namespace exercise_49
   {
     public static void Main(string[] args)
     {
-      // Call your method here:
+      // Call your method here
+      PrintPhrase();
  
     }
 
     // Write your method here:
     public static void PrintPhrase() {
-
+      Console.WriteLine("In a hole in the ground there lived a method");
+      Console.WriteLine();
     }
   }
 }
--- part2/moreLoops/exercise_48/Program.cs
+++ part2/moreLoops/exercise_48/Program.cs
@@ -8,6 +8,26 @@ namespace exercise_48
     {
 
       // Write your code here:
+      int userInput = 0;
+      int sumInput = 0;
+      int countInput = 0;
+      int evenNumber = 0;
+      Console.WriteLine("Give Numbers:");
+      while (userInput !=-1)
+      {
+        userInput = Convert.ToInt32(Console.ReadLine());
+        if (userInput !=-1)
+        {
+          sumInput = sumInput + userInput;
+          countInput = countInput + 1;
+          if (userInput % 2 == 0)
+          {
+            evenNumber = evenNumber +1;
+          }
+        }
+      }
+      double average = sumInput/Convert.ToDouble(countInput);
+      Console.WriteLine("Thx! Bye!\nSum: " + sumInput + "\nNumbers: " + countInput + "\nAverage: "+average+ "\nEven: "+evenNumber+ "\nOdd: "+(countInput-evenNumber));
  
     }
   }
