4f75bf2d8efaa2509e8f79644d4bd786cb8dc24e
JT Sousa <joao.sousa@centria.fi>
2020-02-02 21:33:21 +0200

    exercise 60 done

--- part2/methods/exercise_60/Program.cs
+++ part2/methods/exercise_60/Program.cs
@@ -6,12 +6,19 @@ namespace exercise_60
   {
         public static void Main(String[] args)
     {
-
-    }
+      PrintTriangle(4);
+    } 
 
 
     public static void PrintStars(int number)
     {
+      int i=0;
+      while (i<number)
+      {
+        Console.Write("*");
+        i++;
+      }
+      Console.WriteLine("");
       // you can print one star with the command
       // Console.Write("*");
       // call the print command n times
@@ -21,17 +28,51 @@ namespace exercise_60
 
     public static void PrintSquare(int size)
     {
-
+      int h=0;
+      while (h<size)
+      {
+        int w=0;
+        while (w<size)
+        {
+          Console.Write("*");
+          w++;
+        }
+        h++;
+        Console.WriteLine("");
+      }   
     }
 
     public static void PrintRectangle(int width, int height)
     {
-
+      int h=0;
+      while (h<height)
+      {
+        int w=0;
+        while (w<width)
+        {
+          Console.Write("*");
+          w++;
+        }
+        h++;
+        Console.WriteLine("");
+      }   
     }
 
     public static void PrintTriangle(int size)
     {
-
+      int h=0;
+      while (h<size)
+      {
+        int w=0;
+        while (w<=h)
+        {
+          Console.Write("*");
+          w++;
+        }
+        h++;
+        Console.WriteLine("");
+        
+      }   
     }
   }
 }
