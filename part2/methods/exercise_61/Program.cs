59ce71b9fbec9b62d811300218ed64b3032cfa64
JT Sousa <joao.sousa@centria.fi>
2020-02-02 22:51:56 +0200

    exercise 61 done

--- part2/methods/exercise_61/Program.cs
+++ part2/methods/exercise_61/Program.cs
@@ -6,27 +6,58 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
-
+      ChristmasTree(10);
     }
 
     public static void PrintStars(int number)
     {
-
+      int i=0;
+      while (i<number)
+      {
+        Console.Write("*");
+        i++;
+      }
+      Console.WriteLine("");
     }
 
     public static void PrintSpaces(int number)
     {
-
+      int i=0;
+      while (i<number)
+      {
+        Console.Write(" ");
+        i++;
+      }
     }
 
     public static void PrintRightTriangle(int size)
     {
-
+        int w=1;
+        while (w<=size)
+        {
+          PrintSpaces(size-w);
+          PrintStars(w);
+          w++;
+        }      
     }
 
     public static void ChristmasTree(int height)
     {
-
+      int i=1;
+      int s=1;
+        while (i<=height)
+        {
+          PrintSpaces(height-i);
+          PrintStars(s);
+          i++;
+          s=s+2;
+        }  
+      for (int t = 0; t < 2; t++)
+      {  
+        PrintSpaces((s/2)-2);
+        PrintStars(3);
+      } 
     }
+    
   }
 }
