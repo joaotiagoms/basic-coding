eeb52ad6681266fd8130c3b910de3633803e24b4
JT Sousa <joao.sousa@centria.fi>
2020-02-06 10:44:43 +0200

    exercise 72 done

--- part3/lists/exercise_72/Program.cs
+++ part3/lists/exercise_72/Program.cs
@@ -7,6 +7,35 @@ namespace exercise_72
   {
     public static void Main(string[] args)
     {
+      List<int> list = new List<int>();
+      while (true)
+      {
+        int input = Convert.ToInt32(Console.ReadLine());
+        if (input == 9999)
+        {
+          break;
+        }
+        list.Add(input);
+      }
+      int smallest = list[0];
+
+      for (int i = 0; i < list.Count; i++)
+      {
+          int number = list[i];
+          if (smallest > number)
+          {
+            smallest = number;
+          }
+      }
+      Console.WriteLine("Smallest number: " + smallest);
+
+      for (int i = 0; i < list.Count; i++)
+      {  
+        if (smallest == list[i])
+        {
+        Console.WriteLine("Found at index: " + i);
+        }
+      } 
 
     }
   }
