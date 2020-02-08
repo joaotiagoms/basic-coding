fb91be7345bd774be2cdf71e51e1d665037a1191
JT Sousa <joao.sousa@centria.fi>
2020-02-08 10:59:19 +0200

    exercise 80 done

--- part3/arrays/exercise_80/Program.cs
+++ part3/arrays/exercise_80/Program.cs
@@ -14,8 +14,12 @@ namespace exercise_80
     }
 
     public static int SumOfNumbersInArray(int[] numbers) {
-
-      return 0;
+     int sum = 0;
+      foreach (int number in numbers)
+      {
+        sum = sum + number;
+      }
+      return sum;
     }
 
   }
