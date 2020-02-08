2c55a05d35b31b827f8282bc4046a8722ae00adc
JT Sousa <joao.sousa@centria.fi>
2020-02-08 12:17:11 +0200

    exercise 82 done

--- part3/arrays/exercise_82/Program.cs
+++ part3/arrays/exercise_82/Program.cs
@@ -15,7 +15,16 @@ namespace exercise_82
 
     public static void PrintArrayInStars(int[] array)
     {
-
+      foreach (int i in array) //for each array number
+      {
+        int l=0;
+      while (l<i) 
+      {
+        Console.Write("*");
+        l++;
+      }
+      Console.WriteLine("");
+      }
     }
   }
 }
