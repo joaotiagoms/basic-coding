a7afe128683342c2ade5089b28c2e59fac1e53b9
JT Sousa <joao.sousa@centria.fi>
2020-02-08 22:26:00 +0200

    exercise 85 done

--- part3/strings/exercise_85/Program.cs
+++ part3/strings/exercise_85/Program.cs
@@ -7,6 +7,18 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Enter username:");
+      string username = Console.ReadLine();
+      Console.WriteLine("Enter password:");
+      string password = Console.ReadLine();
+      if ((username == "alex" && password == "sunshine") || (username == "emma" && password == "haskell"))
+      {
+          Console.WriteLine("You have successfully logged in!");
+      }
+      else
+      {
+          Console.WriteLine("Incorrect username or password!");
+      }
 
 
     }
