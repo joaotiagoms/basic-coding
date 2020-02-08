4b54f1d8ed63e04ff1fed3b99fd293a590def891
JT Sousa <joao.sousa@centria.fi>
2020-02-09 00:18:44 +0200

    exercise 87 done

--- part3/strings/exercise_87/Program.cs
+++ part3/strings/exercise_87/Program.cs
@@ -7,7 +7,22 @@ namespace exercise_87
   {
     public static void Main(string[] args)
     {
-
+    while (true)
+    {
+      string input = Console.ReadLine();
+      if (input == "")
+      {
+        break;
+      }
+      string[] pieces = input.Split(" ");
+      for (int i = 0; i < pieces.Length; i++)
+      {
+        if (pieces[i].Contains("av")) 
+      {
+        Console.WriteLine(pieces[i]);
+      }
+      }
+    }
     }
 
   }
