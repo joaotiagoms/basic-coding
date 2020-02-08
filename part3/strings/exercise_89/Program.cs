5eb4580c757647591b6a7cb630259a62305e5d0d
JT Sousa <joao.sousa@centria.fi>
2020-02-09 00:27:28 +0200

    exercise 89 done

--- part3/strings/exercise_89/Program.cs
+++ part3/strings/exercise_89/Program.cs
@@ -7,7 +7,16 @@ namespace exercise_89
   {
     public static void Main(string[] args)
     {
-
+    while (true)
+    {
+      string input = Console.ReadLine();
+      if (input == "")
+      {
+        break;
+      }
+      string[] pieces = input.Split(" ");
+        Console.WriteLine(pieces[pieces.Length-1]);
+    }
 
     }
   }
