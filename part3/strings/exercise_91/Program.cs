87ee26b45885fdb64f2755d75386164b91226648
JT Sousa <joao.sousa@centria.fi>
2020-02-09 02:32:09 +0200

    exercise 91 done

--- part3/strings/exercise_91/Program.cs
+++ part3/strings/exercise_91/Program.cs
@@ -7,7 +7,26 @@ namespace exercise_91
   {
     public static void Main(string[] args)
     {
+    int oldest = 0;
+    string name = "0";
 
+    while (true)
+    {
+      string input = Console.ReadLine();
+      if (input == "")
+      {
+        break;
+      }
+      string[] parts = input.Split(",");
+
+      int number = Convert.ToInt32(parts[1]);
+      if (oldest < number)
+      {
+          oldest = number;
+          name = parts[0];
+      }
+    }
+    Console.WriteLine("Name of the oldest: " + name);
     }
   }
 }
