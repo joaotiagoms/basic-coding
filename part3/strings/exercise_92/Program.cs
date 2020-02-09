5c4da0e56f46e5bef3f23e62be0a2c6dcb09551e
JT Sousa <joao.sousa@centria.fi>
2020-02-09 23:27:26 +0200

    exercise 92 done

--- part3/strings/exercise_92/Program.cs
+++ part3/strings/exercise_92/Program.cs
@@ -7,7 +7,33 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
+      string longest = "";
+      int oldest = 0;
 
+    while (true)
+    {
+      string input = Console.ReadLine();
+      if (input == "")
+      {
+      break;
+      }
+    string[] parts = input.Split(",");
+    
+    string currentName = parts[0];
+    if (longest.Length < currentName.Length)
+      {
+      longest = currentName;
+      }
+
+    int year = Convert.ToInt32(parts[1]);
+      if (oldest < (2020-year))
+      {
+          oldest = (2020-year);
+      }
+
+    }
+    Console.WriteLine("Longest name: "+longest);
+    Console.WriteLine("Highest age: " + oldest);
     }
   }
 }
