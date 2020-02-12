95060a59e4f3bcc1dcad05446586d484a05fe217
JT Sousa <joao.sousa@centria.fi>
2020-02-12 12:59:01 +0200

    exercise 94 done

--- part4/objects/exercise_93/Program.cs
+++ part4/objects/exercise_93/Program.cs
@@ -8,9 +8,11 @@ namespace exercise_93
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+      
     Account heikkisAccount = new Account("Heikki's account", 100.00);
     heikkisAccount.Deposit(20);
     Console.WriteLine(heikkisAccount.balance);    
+    
     }
   }
 }
