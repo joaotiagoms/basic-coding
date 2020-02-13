094636ce90b9373571f61dc0de7a6e60fd56cae5
JT Sousa <joao.sousa@centria.fi>
2020-02-14 00:18:22 +0200

    exercise 102 done

---
+++ part4/objects/exercise_102/Gauge.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace exercise_102
+{
+    public class Gauge
+    {
+        public int value;
+        public Gauge()
+        {
+            this.value = 0;
+        }
+
+        public void Increase()
+        {
+            if (this.value < 5)
+            {
+            this.value = this.value +1;
+            }
+        }
+
+        public void Decrease()
+        {
+            if (this.value > 0)
+            {
+                this.value = this.value -1;
+            }
+        }
+        public bool Full()
+        {
+        if (this.value == 5)
+            {
+              return true;
+            }
+         return false;
+        }
+    }
+}
