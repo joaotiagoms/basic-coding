26ba02835d94ec6732c9504fa02aa2e8f694d927
JT Sousa <joao.sousa@centria.fi>
2020-02-19 12:23:12 +0200

    exercise 106 done

---
+++ part4/objects/exercise_106/PaymentCard.cs
@@ -0,0 +1,18 @@
+namespace exercise_106
+{
+  public class PaymentCard
+  {
+    private double balance;
+
+    public PaymentCard(double openingBalance)
+    {
+      // write code here
+      this.balance = openingBalance;
+    }
+
+    public override string ToString()
+    {
+      return "The card has a balance of " + this.balance + " euros";
+    }
+  }
+}
