09433a74bbb522d39cb6167c083cf035c406cf95
JT Sousa <joao.sousa@centria.fi>
2020-02-21 12:42:03 +0200

    exercise 117 done

--- part4/files/exercise_117/Program.cs
+++ part4/files/exercise_117/Program.cs
@@ -8,8 +8,11 @@ namespace exercise_117
     public static void Main(string[] args)
     {
       // Ask the user for the file name and print the content of the file
+      Console.WriteLine("Which file should have its contents printed?");
+      string userInput = Console.ReadLine();
+      string textFile = File.ReadAllText(userInput);
+      Console.WriteLine(textFile);
 
-      
     }
   }
 }
