2e9c7ec165929f5991be51275b3fe92c7ae367f0
JT Sousa <joao.sousa@centria.fi>
2020-02-21 14:00:54 +0200

    exercise 119 done

---
+++ part5/revision/exercise_119/Timer.cs
@@ -0,0 +1,31 @@
+using System;
+namespace exercise_119
+{
+public class Timer
+{
+  private ClockHand seconds;
+  private ClockHand hundreths;
+
+  public Timer()
+  {
+    //this.hours = new ClockHand(24);
+    this.seconds = new ClockHand(60);
+    this.hundreths = new ClockHand(100);
+  }
+
+  public void Advance()
+  {
+    this.hundreths.Advance();
+
+      if (this.hundreths.value == 0)
+      {
+        this.seconds.Advance();
+      }
+
+  }
+  public override string ToString()
+  {
+    return seconds + ":" + hundreths;
+  }
+}
+}
