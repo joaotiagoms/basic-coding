17907959e70a817c483744876daa698f6e5e7c3c
JT Sousa <joao.sousa@centria.fi>
2020-02-29 14:41:37 +0200

    exercise 122 done

--- part5/overloading/exercise_122/Product.cs
+++ part5/overloading/exercise_122/Product.cs
@@ -9,6 +9,25 @@ namespace exercise_122
 
     // CREATE YOUR CONSTRUCTORS HERE
 
+    public Product(string name)
+    {
+      this.name=name;
+      this.location="shelf";
+      this.weight=1;
+    }
+    public Product(string name, string location)
+    {
+      this.name=name;
+      this.location=location;
+      this.weight=1;
+    }
+    public Product(string name, int weight)
+    {
+      this.name=name;
+      this.location="warehouse";
+      this.weight=weight;
+    }
+
 
 
 
