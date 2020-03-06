7d7d27d33126fea900c34c89a8616c1034a361ae
JT Sousa <joao.sousa@centria.fi>
2020-03-06 11:46:56 +0200

    exercise 124 done

--- part5/references/exercise_124/Program.cs
+++ part5/references/exercise_124/Program.cs
@@ -2,12 +2,30 @@
 
 namespace exercise_124
 {
+  class anyClass
+  {
+    private int value;
+  public anyClass(int value)
+  {
+    this.value=value;
+  }
+    public void addValue()
+    {
+      this.value += 1;
+    }
+  }
   class Program
   {
     public static void Main(string[] args)
     {
       // Implement your program here!
+    anyClass newobject = new anyClass(3);
+    Console.WriteLine(newobject);
 
+    newobject = null;
+    newobject.addValue();
+    
+      
       
     }
   }
